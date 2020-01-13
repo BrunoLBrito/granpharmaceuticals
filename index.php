8e45dbc337a5fb120ae5cb03fa0e6f263bb9ef1d
Bruno Brito <bruno-lucas@msn.com>
2020-01-12 22:41:31 -0300

    site em manutencao

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Excelência é o principal que a Gran Pharmaceuticals representa. A Gran Pharmaceuticals estabeleceu sua reputação com a excelente fabricante de medicamentos para o melhor alto rendimento.">
+    <meta name="robots" content="index, follow">
+    <meta itemprop="name" content="Excelência é o principal que a Gran Pharmaceuticals representa. A Gran Pharmaceuticals estabeleceu sua reputação com a excelente fabricante de medicamentos para o melhor alto rendimento.">
+    <meta itemprop="description" content="Excelência é o principal que a Gran Pharmaceuticals representa. A Gran Pharmaceuticals estabeleceu sua reputação com a excelente fabricante de medicamentos para o melhor alto rendimento.">
+    <meta name="google-site-verification" content="6ZXEdVTeAeTt8lbfo1MFcbENIcy9RDK6Qvin6ju3Y0I" />
+    <link rel="base" href="https://www.granpharmaceuticals.com.br">
+    <link rel="canonical" href="https://www.granpharmaceuticals.com.br/">
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700|Open+Sans:300,400,700|Oswald:300,500,700|Roboto:100,300,400,500,700" rel="stylesheet">
+    <link rel="stylesheet" href="app/assets/css/style.css">
+    <link rel="stylesheet" href="app/assets/css/animate.css">
+    <title>Gran Pharmaceuticals - Excelência em Pharmacos</title>
+    <style>
+       h1 {
+          font-size: 3em;
+          text-align: center !important;
+          color: #F4C200;
+          margin-top: 50px;
+       }
+
+       .img-manu {
+          width: 90%;
+          margin: 0px auto;
+          text-align: center;
+       }
+
+       .img-manu img {
+          width: 100%;
+       }
+
+       p {
+          text-align: center;
+          font-size: 1.5em;
+          color: #fff;
+       }
+
+       p a {
+          color: tomato;
+       }
+    </style>
+</head>
+<body>
+
+    <div>
+
+         <h1>Gran Pharmaceuticals</h1>
+         <h2>Site em manutenção</h2>
+
+         <div class="img-manu">
+            <img src="./app/assets/img/manutencao.png" alt="Gran Pharmaceuticals">
+         </div>
+         
+         <p>Entre em contato através do instagram <a href="https://www.instagram.com/gran.pharmaceuticals/">gran.pharmaceuticals
+</a></p>
+    </div>
+</body>
+</html>
